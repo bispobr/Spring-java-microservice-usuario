f59809648bdee97fe693852c9c2a55d22a66e75c
bispobr <156684382+bispobr@users.noreply.github.com>
2025-03-17 15:41:11 -0300

    feat: adiciona repository

---
+++ src/main/java/com/microservice/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.usuario.repository;
+
+import com.microservice.usuario.models.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
+}
