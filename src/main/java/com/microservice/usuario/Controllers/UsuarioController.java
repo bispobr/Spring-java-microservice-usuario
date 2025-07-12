ffe0646b38cf1864a9ca4873398d321403ef948c
bispobr <156684382+bispobr@users.noreply.github.com>
2025-07-12 16:52:05 -0300

    feat: Add  anotação Swagger e logging ao UsuarioController
    Introduzidas anotações OpenAPI do Swagger para documentar o endpoint POST /usuario, especificando códigos de resposta e descrições. Adicionado Lombok @Slf4j para registro e incluído um log de informações quando uma solicitação de registro de usuário é recebida.

--- src/main/java/com/microservice/usuario/Controllers/UsuarioController.java
+++ src/main/java/com/microservice/usuario/Controllers/UsuarioController.java
@@ -3,7 +3,10 @@ package com.microservice.usuario.Controllers;
 import com.microservice.usuario.dtos.UsuarioRecordDto;
 import com.microservice.usuario.models.UsuarioModel;
 import com.microservice.usuario.services.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Slf4j
 @Controller
 public class UsuarioController {
 
@@ -19,7 +23,12 @@ public class UsuarioController {
     UsuarioService usuarioService;
 
     @PostMapping("/usuario")
+    @Operation(description = "Endpoint responsável por cadastrar novos Usuários")
+    @ApiResponse(responseCode = "201", description = "Usuário cadastrado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição, dados enviados não atendem os requisitos")
+    @ApiResponse(responseCode = "500", description = "Erro interno")
     public ResponseEntity<UsuarioModel> salvarUsuario(@RequestBody @Valid UsuarioRecordDto usuarioRecordDto) {
+        log.info("Solicitação de Cadastro de Usuário recebida");
         var usuarioModel = new UsuarioModel();
         BeanUtils.copyProperties(usuarioRecordDto, usuarioModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.Salvar(usuarioModel));
